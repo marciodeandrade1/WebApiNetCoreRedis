8b72036de7828485cbb206a6584be4745130bbf2
Marcio de Andrade <marcio@requestdev.net>
2024-07-04 16:59:13 -0400

    camada de persistência no banco InMemory

--- WebApiNetCoreRedis/Infra/Persistence/ToDoListDbContext.cs
+++ WebApiNetCoreRedis/Infra/Persistence/ToDoListDbContext.cs
@@ -1,6 +1,18 @@
-﻿namespace WebApiNetCoreRedis.Infra.Persistence
+﻿using WebApiNetCoreRedis.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiNetCoreRedis.Infra.Persistence
 {
-    public class ToDoListDbContext
+    public class ToDoListDbContext : DbContext
     {
+        public ToDoListDbContext(DbContextOptions<ToDoListDbContext> options) : base(options) { }
+
+        public DbSet<ToDo> ToDos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ToDo>()
+                .HasKey(t => t.Id);
+        }
     }
 }
